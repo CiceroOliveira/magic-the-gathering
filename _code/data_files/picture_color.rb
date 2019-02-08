8cf6afef32032c25d8eb32477acade81290c21da
Cicero Oliveira <cicero.oliveira@rsagroup.ca>
2019-02-08 12:43:52 -0500

    Color thief

--- _code/data_files/picture_color.rb
+++ _code/data_files/picture_color.rb
@@ -1,5 +1,5 @@
 require 'miro'
 
-Miro.options[:color_count] = 6
+Miro.options[:color_count] = 3
 colors = Miro::DominantColors.new '../../assets/img/prerelease_pack_azorius.png'
-puts colors.to_hex
+puts colors.to_rgb
--- _posts/2019-01-30-ravnica-allegiance-sets.md
+++ _posts/2019-01-30-ravnica-allegiance-sets.md
@@ -2,5 +2,5 @@
 layout: deck_sets
 title:  "Ravnica Allegiance Sets"
 date:   2019-01-30 11:20:00 -0500
-categories: mtg ravinca_allegiance sets
+categories: mtg ravnica_allegiance sets
 ---
