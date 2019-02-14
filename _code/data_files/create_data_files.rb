73e728607ba61599181038d579cb44f83116058a
Cicero Oliveira <cicero.oliveira@rsagroup.ca>
2019-02-14 15:57:14 -0500

    sass colors

--- _code/data_files/create_data_files.rb
+++ _code/data_files/create_data_files.rb
@@ -4,8 +4,8 @@ require 'yaml'
 database_name = 'mtg_database.db'
 current_path = File.dirname(__FILE__)
 
-#set_name = 'guilds_of_ravnica'
-set_name = 'ravnica_allegiance'
+set_name = 'guilds_of_ravnica'
+#set_name = 'ravnica_allegiance'
 
 DB = Sequel.sqlite File.join(current_path, '/', database_name)
 
@@ -14,34 +14,54 @@ output_file = File.join(current_path, '/../../_data/', "#{set_name}_packs.yml")
 
 begin
 
-  mtg = YAML.load_file(input_file)
+  packs = YAML.load_file(input_file)
 
-  ds = DB[:mtgjson_cards]
+  @ds = DB[:mtgjson_cards]
 
-  mtg.each do |set|
+  packs.each do |set|
     @cards = Array.new
 
     set["cards"].each do |card_set, cards|
+      @cards_in_set = Hash.new
 
       cards.lstrip.split(", ").each do |card_number|
-        ds = DB[:mtgjson_cards]
-        card = ds.select(:name, :rarity, :layout).where(setCode: card_set, number: card_number).all
+        card = @ds.select(:name, :rarity, :layout, :types).where(setCode: card_set, number: card_number).all
 
         if card.count == 0
-          puts "===> #{card_number}"
-          puts "Not Found"
+          puts "Not Found => #{card_set} - #{card_number}"
         else
           if card[0][:layout].eql? 'split'
             name = "#{card[0][:name]}/#{card[1][:name]}"
           else
             name = card[0][:name]
           end
-          @cards << "(#{card_number}) #{name} - #{card[0][:rarity].capitalize}"
+
+          card_type = YAML.load(card[0][:types])[0]
+
+          if (@cards_in_set[card_type].nil?)
+            @cards_in_set[card_type] = Array.new
+          end
+
+          @cards_in_set[card_type] << {'identifier' => card_set + card_number,'card_set' => card_set, 'number' => card_number, 'name' => name, 'rarity' => card[0][:rarity].capitalize, 'types' => YAML.load(card[0][:types])[0]}
+
         end
+
       end
 
-      set["cards"] = @cards
+      @cards_in_set.each do |key, value|
+        @cards_in_set[key] = value.sort_by {|h| h[:identifier] }.group_by{ |h| h }.map{|k, v| k['count']=v.size; k}
+        # @cards_in_set[key] = Hash[value.group_by{ |h| [h['identifier'], h]}.map{ |k, v| [k, v.count]}]
+        # value = value.uniq.map {|h| h[count: value.count(h)]; h}
+        puts value
+      end
+
+      @cards_in_set.each do |key, value|
+        @cards << {'type' => key, 'count' => value.sum {|h| h['count']},'cards' => value}
+      end
+
     end
+
+    set["cards"] = @cards
 
   end
 
@@ -51,4 +71,4 @@ rescue SQLite3::Exception => e
   puts e
 end
 
-File.write output_file, mtg.to_yaml
+File.write output_file, packs.to_yaml
--- _code/data_files/picture_color.rb
+++ _code/data_files/picture_color.rb
@@ -1,5 +1,26 @@
 require 'miro'
 
-Miro.options[:color_count] = 3
-colors = Miro::DominantColors.new '../../assets/img/prerelease_pack_azorius.png'
-puts colors.to_rgb
+# Miro.options[:color_count] = 3
+# colors = Miro::DominantColors.new '../../assets/img/prerelease_pack_azorius.png'
+# puts colors.to_hex
+
+# Dir.entries('../../assets/img').select do |file|
+#   puts file
+# end
+
+color_file = File.new("../../_sass/colors.scss", 'w')
+
+Dir.glob("../../assets/img/*.{png,jpg}").sort.each do |file|
+  style = File.basename(file, File.extname(file)).downcase.strip
+
+  colors = Miro::DominantColors.new(file).to_hex
+
+  color_file.puts ".#{style} {"
+  # color_file.puts "  color: #{colors[0]};"
+  color_file.puts "  color: #eee;"
+  color_file.puts "  border-color: #{colors[1]};"
+  color_file.puts "  background-color: #{colors[2]};"
+  color_file.puts "}"
+  color_file.puts ""
+
+end
